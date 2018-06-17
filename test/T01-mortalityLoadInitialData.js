e7c286c3b4fc2ebc4ceceaf83b6a92eda4ccf432
Veronica <vela.palomo.29@gmail.com>
2018-06-17 23:01:28 +0000

    kj

--- test/T01-mortalityLoadInitialData.js
+++ test/T01-mortalityLoadInitialData.js
@@ -1,10 +1,10 @@
 describe('Data is loaded', function () {
 	console.log("dentro del load rape data");
 	it('should show 15 data', function (){
-		browser.get('https://sos1718-12.herokuapp.com/#!/mortality')
+		browser.get('https://sos1718-jun-vvp.herokuapp.com/#!/mortality')
 		.then(function (){
 		    
-		    element.all(by.repeater("data in database"))
+		    element.all(by.repeater("stat in mortality"))
 		    .then(function(data){
 		        expect(data.length).toBeGreaterThan(14);
 		    });
