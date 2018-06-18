57df1f620076082baee984debc2ab6c93082924d
Veronica <vela.palomo.29@gmail.com>
2018-06-18 19:05:00 +0000

    kj

--- public/mortality-manager/analytics/integrations/normal1Ctrl.js
+++ public/mortality-manager/analytics/integrations/normal1Ctrl.js
@@ -1,57 +1,58 @@
-/*global Chartist, angular*/
+/*global google, angular*/
 
 angular.module("managerApp").
 controller("normal1Ctrl", ["$scope", "$http", "$httpParamSerializer", function($scope, $http, $httpParamSerializer) {
 
-    
+
     var country = [];
     var suicide = [];
-    var weight = [];
-    var height = [];
+    var transport = [];
+    var cancer = [];
+    var title = [];
+
 
     $http
         .get("api/v1/mortality-stats").then(function(res) {
-               for (var i = 0; i < res.data.length; i++) {
-                    suicide.push(res.data[i].suicide);
-                }
+            for (var i = 0; i < res.data.length; i++) {
+                suicide.push(res.data[i].suicide);
+                transport.push(res.data[i].transport);
+                cancer.push(res.data[i].cancer);
+            }
 
             $http
-                .get("http://api.suredbits.com/nfl/v0/players/brady/tom")
+                .get("https://www.metaweather.com/api/location/search/?query=san")
                 .then(function(response) {
-                   for (var i = 0; i < response.data.length; i++) {
+                    for (var i = 0; i < response.data.length; i++) {
 
-                    weight.push(response.data[i].weight);
-                    height.push(response.data[i].height);
-                }
+                        title.push(response.data[i].title);
 
-                    var data = {
-                        labels: [country[0], height[0]],
-                        series: [suicide[0], weight[0]]
-                    };
+                    }
 
-                    var options = {
-                        labelInterpolationFnc: function(value) {
-                            return value[0]
-                        }
-                    };
+                    google.charts.load('current', { 'packages': ['corechart'] });
+                    google.charts.setOnLoadCallback(drawChart);
 
-                    var responsiveOptions = [
-                        ['screen and (min-width: 640px)', {
-                            chartPadding: 30,
-                            labelOffset: 100,
-                            labelDirection: 'explode',
-                            labelInterpolationFnc: function(value) {
-                                return value;
-                            }
-                        }],
-                        ['screen and (min-width: 1024px)', {
-                            labelOffset: 80,
-                            chartPadding: 20
-                        }]
-                    ];
+                    function drawChart() {
 
-                    new Chartist.Pie('ct-chart', data, options, responsiveOptions);
+                        var data = google.visualization.arrayToDataTable([
+                            
+                            [title[0], suicide[0]],
+                            [title[0], transport[0]],
+                            [title[0], cancer[0]],
+                            [title[1], suicide[1]],
+                            [title[1], transport[1]],
+                            [title[1], cancer[1]],
+                            
+                            
+                        ]);
 
+                        var options = {
+                            title: 'Stats'
+                        };
+
+                        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                        chart.draw(data, options);
+                    }
 
                 });
 
