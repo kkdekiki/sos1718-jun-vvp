de4ac2367a9a5b7507e48cb8602405a8c698a93c
Veronica <vela.palomo.29@gmail.com>
2018-06-18 13:01:46 +0000

    kj

--- public/mortality-manager/analytics/integrations/integration3Ctrl.js
+++ public/mortality-manager/analytics/integrations/integration3Ctrl.js
@@ -12,38 +12,39 @@ angular
 
         $http.get("https://sos1718-10.herokuapp.com/api/v1/buses").then(function(response) {
             res = response.data;
-        });
 
 
-        $http.get("/api/v1/mortality-stats").then(function(response) {
 
-            for (var i = 0; i < response.data.length; i++) {
-                for (var j = 0; j < res.length; j++) {
-                    if (response.data[i].year == parseInt(res[j].year)) {
-                        country.push(response.data[i].country);
-                        occupation.push(res[j].occupation);
-                        cancer.push(response.data[i].cancer);
+            $http.get("/api/v1/mortality-stats").then(function(response) {
+
+                for (var i = 0; i < response.data.length; i++) {
+                    for (var j = 0; j < res.length; j++) {
+                        if (response.data[i].year == parseInt(res[j].year)) {
+                            country.push(response.data[i].country);
+                            occupation.push(res[j].occupation);
+                            cancer.push(response.data[i].cancer);
+                        }
+
                     }
-
                 }
-            }
 
-            var chart = c3.generate({
-                data: {
-                    columns: [
-                        occupation,
-                        cancer
+                var chart = c3.generate({
+                    data: {
+                        columns: [
+                            occupation,
+                            cancer
 
-                    ],
-                    types: {
-                        data1: 'area-spline',
-                        data2: 'area-spline'
+                        ],
+                        types: {
+                            data1: 'area-spline',
+                            data2: 'area-spline'
 
-                    },
-                    groups: [
-                        ['occupation', 'cancer']
-                    ]
-                }
+                        },
+                        groups: [
+                            ['occupation', 'cancer']
+                        ]
+                    }
+                });
             });
         });
     }]);
