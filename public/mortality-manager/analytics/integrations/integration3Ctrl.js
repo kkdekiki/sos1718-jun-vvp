d46ae13ad17e5d5240c5eba8833aa4ad0f456cef
Veronica <vela.palomo.29@gmail.com>
2018-06-18 01:18:58 +0000

    kj

--- public/mortality-manager/analytics/integrations/integration3Ctrl.js
+++ public/mortality-manager/analytics/integrations/integration3Ctrl.js
@@ -6,11 +6,11 @@ angular
     .module("managerApp")
     .controller("integration3Ctrl", ["$scope", "$http", function($scope, $http) {
         var res = [];
-        var population = ['population'];
+        var occupation = ['occupation'];
         var cancer = ['cancer'];
         var country = ['data3'];
 
-        $http.get("https://sos1718-13.herokuapp.com/api/v1/gpi-stats").then(function(response) {
+        $http.get("https://sos1718-10.herokuapp.com/api/v1/buses").then(function(response) {
             res = response.data;
         });
 
@@ -21,7 +21,7 @@ angular
                 for (var j = 0; j < res.length; j++) {
                     if (response.data[i].year == parseInt(res[j].year)) {
                         country.push(response.data[i].country);
-                        population.push(res[j].population);
+                        occupation.push(res[j].occupation);
                         cancer.push(response.data[i].cancer);
                     }
 
@@ -31,7 +31,7 @@ angular
             var chart = c3.generate({
                 data: {
                     columns: [
-                        population,
+                        occupation,
                         cancer
 
                     ],
@@ -41,7 +41,7 @@ angular
 
                     },
                     groups: [
-                        ['population', 'cancer']
+                        ['occupation', 'cancer']
                     ]
                 }
             });
